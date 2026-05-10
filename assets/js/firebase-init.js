db945e96384d0efea0aba8dd29fbef518a75bbd3
Abhishek333k <mabhishekramesh1@gmail.com>
2026-05-10 20:51:42 +0530

    fix: purged app check and fixed regex crash in firestore rules

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -3,9 +3,6 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-storage.js";
 
-// STRICT V3 IMPORT
-import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app-check.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAVZHO-avENaKejMjAUexsaem-Dusljvzo",
   authDomain: "hope-homeo-care.firebaseapp.com",
@@ -16,22 +13,10 @@ const firebaseConfig = {
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-// LOCAL DEV BYPASS
-if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
-    self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
-}
-
-// V3 PROVIDER ENFORCEMENT WITH NEW SITE KEY
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6LfodbwsAAAAAIeocZ4hd28rL9bp7hZNaxaIi9Yu'),
-  isTokenAutoRefreshEnabled: true 
-});
-
-const auth = getAuth(app);
+const auth = typeof getAuth === "function" ? getAuth(app) : null;
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-auth.useDeviceLanguage();
+if (auth) auth.useDeviceLanguage();
 
 export { app, auth, db, storage };
